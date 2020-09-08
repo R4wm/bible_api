b87a6f5e42507785b9fcbf602098fe9abc32f79f
r4wm <raymondmintz11@gmail.com>
2020-09-07 23:43:51 -0700

    color code graph

--- kjv/templates.go
+++ kjv/templates.go
@@ -187,21 +187,21 @@ const (
 	searchResultTemplate = `
 <!DOCTYPE html>
 <html>
-<canvas id="myChart" width="400" height="400"></canvas>
+  <canvas id="myChart" width="400" height="400"></canvas>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js" integrity="sha512-s+xg36jbIujB2S2VKfpGmlC3T5V2TF3lY48DX7u2r9XzGzgPsa6wTpOQA7J9iffvdeBN0q9tKzRxVxw1JviZPg==" crossorigin="anonymous"></script>
-<body>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js" integrity="sha512-s+xg36jbIujB2S2VKfpGmlC3T5V2TF3lY48DX7u2r9XzGzgPsa6wTpOQA7J9iffvdeBN0q9tKzRxVxw1JviZPg==" crossorigin="anonymous"></script>
+  <body>
 
-  {{range .Verses }}
+    {{range .Verses }}
 
-  <center>
-    <p> <a href="{{ createLink .}}">{{ .Book }} {{ .Chapter }}:{{ .Verse}} </a></p>
-    <p> {{ .Text }} </p>
+    <center>
+      <p> <a href="{{ createLink .}}">{{ .Book }} {{ .Chapter }}:{{ .Verse}} </a></p>
+      <p> {{ .Text }} </p>
 
-  </center>
-  {{ end }}
-<canvas id="myChart" width="10" height="10"></canvas>
-<script>
+    </center>
+    {{ end }}
+    <canvas id="myChart" width="10" height="10"></canvas>
+    <script>
 var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'bar',
@@ -279,19 +279,141 @@ var myChart = new Chart(ctx, {
 	    data: {{.GraphCount}},
 	    backgroundColor: [
 		'rgba(255, 99, 132, 0.2)',
-		'rgba(54, 162, 235, 0.2)',
-		'rgba(255, 206, 86, 0.2)',
+	'rgba(255, 99, 132, 0.2)',
+	'rgba(255, 99, 132, 0.2)',
+	'rgba(255, 99, 132, 0.2)',
+	'rgba(255, 99, 132, 0.2)',
+	'rgba(255, 99, 132, 0.2)',
+	'rgba(255, 99, 132, 0.2)',
+	'rgba(255, 99, 132, 0.2)',
+	'rgba(255, 99, 132, 0.2)',
+	'rgba(255, 99, 132, 0.2)',
+	'rgba(255, 99, 132, 0.2)',
+	'rgba(255, 99, 132, 0.2)',
+	'rgba(255, 99, 132, 0.2)',
+	'rgba(255, 99, 132, 0.2)',
+	'rgba(255, 99, 132, 0.2)',
+	'rgba(255, 99, 132, 0.2)',
+	'rgba(255, 99, 132, 0.2)',
+	'rgba(255, 99, 132, 0.2)',
+	'rgba(255, 99, 132, 0.2)',
+	'rgba(255, 99, 132, 0.2)',
+	'rgba(255, 99, 132, 0.2)',
+	'rgba(255, 99, 132, 0.2)',
+	'rgba(255, 99, 132, 0.2)',
+	'rgba(255, 99, 132, 0.2)',
+	'rgba(255, 99, 132, 0.2)',
+	'rgba(255, 99, 132, 0.2)',
+	'rgba(255, 99, 132, 0.2)',
+	'rgba(255, 99, 132, 0.2)',
+	'rgba(255, 99, 132, 0.2)',
+	'rgba(255, 99, 132, 0.2)',
+	'rgba(255, 99, 132, 0.2)',
+	'rgba(255, 99, 132, 0.2)',
+	'rgba(255, 99, 132, 0.2)',
+	'rgba(255, 99, 132, 0.2)',
+	'rgba(255, 99, 132, 0.2)',
+	'rgba(255, 99, 132, 0.2)',
+	'rgba(255, 99, 132, 0.2)',
+	'rgba(255, 99, 132, 0.2)',
+	'rgba(255, 99, 132, 0.2)',
 		'rgba(75, 192, 192, 0.2)',
-		'rgba(153, 102, 255, 0.2)',
-		'rgba(255, 159, 64, 0.2)'
+		'rgba(75, 192, 192, 0.2)',
+		'rgba(75, 192, 192, 0.2)',
+		'rgba(75, 192, 192, 0.2)',
+		'rgba(75, 192, 192, 0.2)',
+		'rgba(75, 192, 192, 0.2)',
+		'rgba(75, 192, 192, 0.2)',
+		'rgba(75, 192, 192, 0.2)',
+		'rgba(75, 192, 192, 0.2)',
+		'rgba(75, 192, 192, 0.2)',
+		'rgba(75, 192, 192, 0.2)',
+		'rgba(75, 192, 192, 0.2)',
+		'rgba(75, 192, 192, 0.2)',
+		'rgba(75, 192, 192, 0.2)',
+		'rgba(75, 192, 192, 0.2)',
+		'rgba(75, 192, 192, 0.2)',
+		'rgba(75, 192, 192, 0.2)',
+		'rgba(75, 192, 192, 0.2)',
+		'rgba(75, 192, 192, 0.2)',
+		'rgba(75, 192, 192, 0.2)',
+		'rgba(75, 192, 192, 0.2)',
+		'rgba(75, 192, 192, 0.2)',
+		'rgba(75, 192, 192, 0.2)',
+		'rgba(75, 192, 192, 0.2)',
+		'rgba(75, 192, 192, 0.2)',
+		'rgba(75, 192, 192, 0.2)',
+		'rgba(75, 192, 192, 0.2)',
+		'rgba(54, 162, 235, 0.2)'
 	    ],
 	    borderColor: [
 		'rgba(255, 99, 132, 1)',
-		'rgba(54, 162, 235, 1)',
-		'rgba(255, 206, 86, 1)',
+		'rgba(255, 99, 132, 1)',
+		'rgba(255, 99, 132, 1)',
+		'rgba(255, 99, 132, 1)',
+		'rgba(255, 99, 132, 1)',
+		'rgba(255, 99, 132, 1)',
+		'rgba(255, 99, 132, 1)',
+		'rgba(255, 99, 132, 1)',
+		'rgba(255, 99, 132, 1)',
+		'rgba(255, 99, 132, 1)',
+		'rgba(255, 99, 132, 1)',
+		'rgba(255, 99, 132, 1)',
+		'rgba(255, 99, 132, 1)',
+		'rgba(255, 99, 132, 1)',
+		'rgba(255, 99, 132, 1)',
+		'rgba(255, 99, 132, 1)',
+		'rgba(255, 99, 132, 1)',
+		'rgba(255, 99, 132, 1)',
+		'rgba(255, 99, 132, 1)',
+		'rgba(255, 99, 132, 1)',
+		'rgba(255, 99, 132, 1)',
+		'rgba(255, 99, 132, 1)',
+		'rgba(255, 99, 132, 1)',
+		'rgba(255, 99, 132, 1)',
+		'rgba(255, 99, 132, 1)',
+		'rgba(255, 99, 132, 1)',
+		'rgba(255, 99, 132, 1)',
+		'rgba(255, 99, 132, 1)',
+		'rgba(255, 99, 132, 1)',
+		'rgba(255, 99, 132, 1)',
+		'rgba(255, 99, 132, 1)',
+		'rgba(255, 99, 132, 1)',
+		'rgba(255, 99, 132, 1)',
+		'rgba(255, 99, 132, 1)',
+		'rgba(255, 99, 132, 1)',
+		'rgba(255, 99, 132, 1)',
+		'rgba(255, 99, 132, 1)',
+		'rgba(255, 99, 132, 1)',
+		'rgba(255, 99, 132, 1)',
 		'rgba(75, 192, 192, 1)',
-		'rgba(153, 102, 255, 1)',
-		'rgba(255, 159, 64, 1)'
+		'rgba(75, 192, 192, 1)',
+		'rgba(75, 192, 192, 1)',
+		'rgba(75, 192, 192, 1)',
+		'rgba(75, 192, 192, 1)',
+		'rgba(75, 192, 192, 1)',
+		'rgba(75, 192, 192, 1)',
+		'rgba(75, 192, 192, 1)',
+		'rgba(75, 192, 192, 1)',
+		'rgba(75, 192, 192, 1)',
+		'rgba(75, 192, 192, 1)',
+		'rgba(75, 192, 192, 1)',
+		'rgba(75, 192, 192, 1)',
+		'rgba(75, 192, 192, 1)',
+		'rgba(75, 192, 192, 1)',
+		'rgba(75, 192, 192, 1)',
+		'rgba(75, 192, 192, 1)',
+		'rgba(75, 192, 192, 1)',
+		'rgba(75, 192, 192, 1)',
+		'rgba(75, 192, 192, 1)',
+		'rgba(75, 192, 192, 1)',
+		'rgba(75, 192, 192, 1)',
+		'rgba(75, 192, 192, 1)',
+		'rgba(75, 192, 192, 1)',
+		'rgba(75, 192, 192, 1)',
+		'rgba(75, 192, 192, 1)',
+		'rgba(75, 192, 192, 1)',
+		'rgba(75, 192, 192, 1)'
 	    ],
 	    borderWidth: 1
 	}]
