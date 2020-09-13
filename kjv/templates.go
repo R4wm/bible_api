fef901f49efe6ff86bbe8e314e8a7bfc258d41a9
r4wm <raymondmintz11@gmail.com>
2020-09-13 00:16:31 -0700

    adding html search bar, not good with html

--- kjv/templates.go
+++ kjv/templates.go
@@ -185,8 +185,77 @@ const (
 `
 
 	searchResultTemplate = `
+<!-- SEARCH BAR -->
 <!DOCTYPE html>
 <html>
+  <head>
+    <title>Bible Search</title>
+    <meta name="ROBOTS" content="NOINDEX, NOFOLLOW" />
+    <!-- CSS styles for standard search box -->
+    <style type="text/css">
+      #tfheader{
+      background-color:#c3dfef;
+      }
+      #tfnewsearch{
+      float:right;
+      padding:20px;
+      }
+      .tftextinput{
+      margin: 0;
+      padding: 5px 15px;
+      font-family: Arial, Helvetica, sans-serif;
+      font-size:14px;
+      border:1px solid #0076a3; border-right:0px;
+      border-top-left-radius: 5px 5px;
+      border-bottom-left-radius: 5px 5px;
+      }
+      .tfbutton {
+      margin: 0;
+      padding: 5px 15px;
+      font-family: Arial, Helvetica, sans-serif;
+      font-size:14px;
+      outline: none;
+      cursor: pointer;
+      text-align: center;
+      text-decoration: none;
+      color: #ffffff;
+      border: solid 1px #0076a3; border-right:0px;
+      background: #0095cd;
+      background: -webkit-gradient(linear, left top, left bottom, from(#00adee), to(#0078a5));
+      background: -moz-linear-gradient(top,  #00adee,  #0078a5);
+      border-top-right-radius: 5px 5px;
+      border-bottom-right-radius: 5px 5px;
+      }
+      .tfbutton:hover {
+      text-decoration: none;
+      background: #007ead;
+      background: -webkit-gradient(linear, left top, left bottom, from(#0095cc), to(#00678e));
+      background: -moz-linear-gradient(top,  #0095cc,  #00678e);
+      }
+      /* Fixes submit button height problem in Firefox */
+      .tfbutton::-moz-focus-inner {
+      border: 0;
+      }
+      .tfclear{
+      clear:both;
+      }
+    </style>
+  </head>
+  <body>
+    <!-- HTML for SEARCH BAR -->
+    <div id="tfheader">
+
+      <form id="tfnewsearch" method="get" action="/bible/search">
+	<input type="text" class="tftextinput" name="q" size="21" maxlength="120"><input type="submit" value="search" class="tfbutton">
+      </form>
+      <div class="tfclear"></div>
+    </div>
+  </body>
+<!-- END SEARCH -->
+<!DOCTYPE html>
+
+<html>
+
   <canvas id="myChart" width="400" height="400"></canvas>
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js" integrity="sha512-s+xg36jbIujB2S2VKfpGmlC3T5V2TF3lY48DX7u2r9XzGzgPsa6wTpOQA7J9iffvdeBN0q9tKzRxVxw1JviZPg==" crossorigin="anonymous"></script>
@@ -429,7 +498,7 @@ var myChart = new Chart(ctx, {
     }
 });
 </script>
-  </div>
+</div>
 </body>
 </html>
 `
